48dabba190036de232d7aba1a3eecdb742f6f23e
Affan Imran <affanimran1303@gmail.com>
2024-10-21 22:56:06 +0500

    FAQ Section Updated

--- src/app/FAQ/page.tsx
+++ src/app/FAQ/page.tsx
@@ -4,85 +4,42 @@ const FAQ = () => {
   return (
     <section className="sm:h-max mt-36" id="FAQSection">
       <div className="h-vh text-[#0c4727] text-center p-20 m-10">
-        <h1 className="text-5xl league-spartan font-bold mt-4">Frequently Asked Questions.</h1>
+        <h1 className="text-5xl league-spartan font-bold mt-4">
+          Frequently Asked Questions.
+        </h1>
         <div className="flex justify-center">
-        <div className="league-spartan text-[#e4e3dc] mt-10 w-2/4">
-          <details className="collapse mt-8 bg-[#16794b] transition duration-1050 rounded-md">
-            <summary className="collapse-title text-xl font-medium">
-              Click to open/close
-            </summary>
-            <div className="collapse-content p-5">
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Praesentium voluptate minus soluta nobis dolorem non deleniti
-                adipisci pariatur debitis, quam molestiae neque laboriosam?
-                Debitis consequuntur facere officia quas veritatis dignissimos
-                enim necessitatibus rem, ipsam culpa facilis, iure perspiciatis
-                totam quos eum? Itaque ut vel quis, sapiente sint eveniet illum
-                nesciunt officiis inventore commodi, labore quaerat cupiditate
-                quos necessitatibus. Voluptas debitis est sed iure asperiores,
-                voluptates nisi magnam eius sunt labore delectus eaque.
-                Asperiores sunt reiciendis sequi cupiditate veniam repellendus
-                at obcaecati, quisquam harum in exercitationem aperiam deserunt
-                minus veritatis cum sit quaerat? Ea, quis commodi quos nesciunt
-                consequatur quibusdam aspernatur?
-              </p>
+          <div className="collapse collapse-arrow w-3/4 mt-8 bg-[#16794b] league-spartan text-[#e4e3dc]">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Click me to show/hide content
             </div>
-          </details>
-        </div>
-        </div>
-        <div className="flex justify-center">
-        <div className="league-spartan text-[#e4e3dc] mt-2 w-2/4">
-          <details className="collapse mt-8 bg-[#16794b] transition duration-1050 rounded-md">
-            <summary className="collapse-title text-xl font-medium">
-              Click to open/close
-            </summary>
-            <div className="collapse-content p-5">
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Praesentium voluptate minus soluta nobis dolorem non deleniti
-                adipisci pariatur debitis, quam molestiae neque laboriosam?
-                Debitis consequuntur facere officia quas veritatis dignissimos
-                enim necessitatibus rem, ipsam culpa facilis, iure perspiciatis
-                totam quos eum? Itaque ut vel quis, sapiente sint eveniet illum
-                nesciunt officiis inventore commodi, labore quaerat cupiditate
-                quos necessitatibus. Voluptas debitis est sed iure asperiores,
-                voluptates nisi magnam eius sunt labore delectus eaque.
-                Asperiores sunt reiciendis sequi cupiditate veniam repellendus
-                at obcaecati, quisquam harum in exercitationem aperiam deserunt
-                minus veritatis cum sit quaerat? Ea, quis commodi quos nesciunt
-                consequatur quibusdam aspernatur?
-              </p>
+            <div className="collapse-content">
+              <p>hello</p>
             </div>
-          </details>
           </div>
         </div>
         <div className="flex justify-center">
-        <div className="league-spartan text-[#e4e3dc] mt-2 w-2/4">
-          <details className="collapse mt-8 bg-[#16794b] transition duration-1050 rounded-md">
-            <summary className="collapse-title text-xl font-medium">
-              Click to open/close
-            </summary>
-            <div className="collapse-content p-5">
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Praesentium voluptate minus soluta nobis dolorem non deleniti
-                adipisci pariatur debitis, quam molestiae neque laboriosam?
-                Debitis consequuntur facere officia quas veritatis dignissimos
-                enim necessitatibus rem, ipsam culpa facilis, iure perspiciatis
-                totam quos eum? Itaque ut vel quis, sapiente sint eveniet illum
-                nesciunt officiis inventore commodi, labore quaerat cupiditate
-                quos necessitatibus. Voluptas debitis est sed iure asperiores,
-                voluptates nisi magnam eius sunt labore delectus eaque.
-                Asperiores sunt reiciendis sequi cupiditate veniam repellendus
-                at obcaecati, quisquam harum in exercitationem aperiam deserunt
-                minus veritatis cum sit quaerat? Ea, quis commodi quos nesciunt
-                consequatur quibusdam aspernatur?
-              </p>
+          <div className="collapse collapse-arrow w-3/4 mt-8 bg-[#16794b] league-spartan text-[#e4e3dc]">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Click me to show/hide content
             </div>
-          </details>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className="flex justify-center">
+          <div className="collapse collapse-arrow w-3/4 mt-8 bg-[#16794b] league-spartan text-[#e4e3dc]">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Click me to show/hide content
+            </div>
+            <div className="collapse-content">
+              <p>hello</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
