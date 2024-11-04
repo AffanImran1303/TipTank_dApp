fb69d379357f36f4cf0ce048e641bdab5022f531
Affan Imran <affanimran1303@gmail.com>
2024-11-04 22:39:28 +0500

    Updated How It Work Section(3)

--- src/app/howitworks/page.tsx
+++ src/app/howitworks/page.tsx
@@ -13,14 +13,24 @@ const HowItWorks = () => {
     [0, 1],
     ["0%", "100%"]
   );
+  const step1Color = useTransform(scrollYProgress, [0.25,0.45], ["", "#6ead3d"]);
+  const step2Color = useTransform(scrollYProgress, [0.35,0.75], ["", "#6ead3d"]);
+  const step3Color = useTransform(scrollYProgress, [0.65,1], ["", "#6ead3d"]);
+  const step4Color = useTransform(scrollYProgress, [0.85,1], ["", "#6ead3d"]);
+
+  const iconStrokeColor1 = useTransform(scrollYProgress, [0.25, 0.45], ["#e4e3dc", "#00302e"]);
+  const iconStrokeColor2 = useTransform(scrollYProgress, [0, 0.5], ["#e4e3dc", "#00302e"]);
+  const iconStrokeColor3 = useTransform(scrollYProgress, [0, 0.65], ["#e4e3dc", "#00302e"]);
+  const iconStrokeColor4 = useTransform(scrollYProgress, [0, 0.65], ["#e4e3dc", "#00302e"]);
+
   return (
     <section
       className="relative w-full max-w-2xl mx-auto py-20 sm:h-max mt-36"
       ref={ref}
       id="HowItWorksSection"
     >
-      <div className="h-screen border-4 border-none rounded-xl text-center p-20 m-10">
-        <h1 className="text-6xl league-spartan font-bold mt-4">
+      <div className="h-screen border-4 border-none rounded-xl text-center m-10">
+        <h1 className="text-7xl league-spartan font-bold mt-4">
           How It Works?
         </h1>
 
@@ -38,8 +48,8 @@ const HowItWorks = () => {
         </div>
         <div className="grid grid-rows-2 gap-40 place-content-evenly">
           <div>
-            <div className="rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-              <svg
+            <motion.div style={{backgroundColor:step1Color}} className="rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+              <motion.svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="50"
                 height="50"
@@ -50,17 +60,18 @@ const HowItWorks = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className="lucide lucide-wallet"
+                style={{stroke:iconStrokeColor1}}
               >
                 <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1" />
                 <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4" />
-              </svg>
-            </div>
+              </motion.svg>
+            </motion.div>
           </div>
-          <p className="absolute mt-8 text-lg">1. Connect Wallet</p>
+          <p className="absolute mt-8 ml-12 text-lg">1. Connect your Wallet</p>
           <div>
             
-            <div className="z-10 rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-              <svg
+            <motion.div style={{backgroundColor:step2Color}} className="z-10 rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+              <motion.svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="50"
                 height="50"
@@ -71,20 +82,21 @@ const HowItWorks = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className="lucide lucide-user-round-search"
+                style={{stroke:iconStrokeColor2}}
               >
                 <circle cx="10" cy="8" r="5" />
                 <path d="M2 21a8 8 0 0 1 10.434-7.62" />
                 <circle cx="18" cy="18" r="3" />
                 <path d="m22 22-1.9-1.9" />
-              </svg>{" "}
+              </motion.svg>{" "}
               
-            </div>
+            </motion.div>
             <p className="absolute mt-8 ml-16 text-lg">2. Search your favourite creator</p>
           </div>
           <div>
           <div className="absolute left-20 mt-8 text-lg">3. Enter the Tip Amount</div>
-            <div className="z-10 rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-              <svg
+            <motion.div style={{backgroundColor:step3Color}} className="z-10 rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+              <motion.svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="50"
                 height="50"
@@ -95,14 +107,15 @@ const HowItWorks = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className="lucide lucide-bitcoin"
+                style={{stroke:iconStrokeColor3}}
               >
                 <path d="M11.767 19.089c4.924.868 6.14-6.025 1.216-6.894m-1.216 6.894L5.86 18.047m5.908 1.042-.347 1.97m1.563-8.864c4.924.869 6.14-6.025 1.215-6.893m-1.215 6.893-3.94-.694m5.155-6.2L8.29 4.26m5.908 1.042.348-1.97M7.48 20.364l3.126-17.727" />
-              </svg>
-            </div>
+              </motion.svg>
+            </motion.div>
           </div>
           <div>
-            <div className="rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
-              <svg
+            <motion.div style={{backgroundColor:step4Color}} className="rounded-full timeline-icon border-[#6ead3d] border-4 w-24 h-24 absolute left-1/2 -translate-y-4 sm:translate-y-0 transform -translate-x-1/2 flex items-center justify-center">
+              <motion.svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="50"
                 height="50"
@@ -113,10 +126,11 @@ const HowItWorks = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className="lucide lucide-check"
+                style={{stroke:iconStrokeColor4}}
               >
                 <path d="M20 6 9 17l-5-5" />
-              </svg>
-            </div>
+              </motion.svg>
+            </motion.div>
             <div className="absolute m-8 ml-16 text-lg">All set, you are done!</div>
           </div>
         </div>
