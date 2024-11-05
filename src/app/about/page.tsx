5c6f962916f61f020d648d6dc25c51b814d16a68
Affan Imran <affanimran1303@gmail.com>
2024-11-05 22:12:46 +0500

    Text Update in About Section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,14 +3,15 @@ import "/public/globals.css";
 
 const About = () => {
   return (
-    <section className="sm:h-max mt-36" id="AboutSection">
-      <div className="h-vh border-4 border-none rounded-xl text-[#fffdff] text-center p-20 m-10">
-        <h1 className="text-5xl league-spartan font-bold mt-4 sm:text-7xl">About TipTank</h1>
-        <div className="text-lg league-spartan">
-      <p className="mt-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque architecto numquam exercitationem veniam maxime autem repudiandae delectus perspiciatis, aspernatur optio perferendis quos sapiente cumque ea deserunt cum assumenda qui laboriosam dicta id similique velit officia? Illum laborum libero velit mollitia distinctio maxime quam error in! Non molestias eum similique laborum.</p>
-      <br/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam molestiae, temporibus aspernatur ex nesciunt accusamus odio magni ab cupiditate animi.</p>
+    <section className="league-spartan sm:h-max mt-36" id="AboutSection">
+      <div className="h-vh grid grid-rows-1 gap-20 place-content-evenly text-[#fffdff] text-center p-20">
+        <h1 className="grid-row text-5xl league-spartan font-bold sm:text-7xl">About TipTank</h1>
+        <div className="text-xl text-[#e3e8e6] leading-loose">
+      <p className="relative z-10">TipTank, your platform for directly supporting the creators you admire! Our mission is to empower artists, writers, musicians, educators, and other creatives by providing a seamless way for fans to show appreciation for their work through tips and contributions.</p>
+
+      <p className="m-12">In today’s fast-paced digital world, creators put countless hours into producing valuable content, often without the security of a steady income. TipTank addresses this by offering a decentralized, user-driven space where fans can easily tip creators as a way to recognize and support their efforts. Unlike traditional platforms, TipTank operates on the blockchain, ensuring transparency, lower fees, and a fairer system that benefits creators more directly.</p>
       </div>
+      {/* <div className="absolute p-20 bg-emerald-500 w-[15%] mt-36"></div> */}
       </div>
 
     </section>
