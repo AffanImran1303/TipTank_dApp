590742a3ea24c07e21eb9fea2eb9c1b95583b1ad
Affan Imran <affanimran1303@gmail.com>
2024-11-26 20:51:00 +0500

    Updated About Section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <section className="league-spartan mt-12 h-max" id="AboutSection">
       <div className="place-content-evenly text-center text-[#fffdff]">
-        <p className="text-4xl league-spartan font-bold transition ease-in-out hover:delay-650 hover:text-[#6ead3d] hover:scale-110 sm:text-5xl md:text-8xl ">
+        <p className="text-4xl league-spartan font-bold transition ease-in-out hover:delay-650 hover:text-[#6ead3d] hover:scale-110 sm:text-5xl md:text-6xl">
           About TipTank
         </p>
         <div className="text-xl leading-loose z-10 font-light p-8 m-2">
-          <p className="">
+          <p className="mx-8">
             TipTank is a blockchain-powered platform designed to help fans
             directly support their favorite creators—artists, writers,
             musicians, educators, and more. In a world where creators invest
@@ -20,7 +20,7 @@ const About = () => {
           </p>
         </div>
       </div>
-      <div className="place-items-center grid grid-cols-1 gap-8 sm:grid-cols-2 text-center leading-loose font-light sm:mt-12">
+      <div className="place-items-center grid grid-cols-1 gap-y-8 lg:grid-cols-2 gap-0 text-center leading-loose font-light sm:mt-12">
         <div className="w-[65%] p-8 border-2 border-[#6ead3d] rounded-xl shadow-2xl">
           <h1 className="text-4xl font-bold p-4 text-center sm:text-5xl">Why TipTank?</h1>
           <p className="text-xl">
